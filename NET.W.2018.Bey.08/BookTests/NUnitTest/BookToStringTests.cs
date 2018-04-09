40afd2cd6cff95915f56446718188a50d569edd6
Irina Bey <irinabey88@gmail.com>
2018-04-10 00:17:32 +0300

    * code refactoring  of unit tests

--- NET.W.2018.Bey.08/BookTests/NUnitTest/BookToStringTests.cs
+++ NET.W.2018.Bey.08/BookTests/NUnitTest/BookToStringTests.cs
@@ -16,26 +16,26 @@ namespace NET.W._2018.Bey._08.Tests.NUnitTests
         [TestCase("ANP", ExpectedResult = "Richter CLR via C# O'REILlY")]
         [TestCase("IANP", ExpectedResult = "978-0735667457 Richter CLR via C# O'REILlY")]
         [TestCase("FI", ExpectedResult = "978-0735667457 Richter CLR via C# O'REILlY 2013 896 176")]
-        public string Book_ToString_ValidData(string format)
+        public string Book_ToString_ValidData_Test(string format)
         {           
             return book.ToString(format);
         }
 
         [TestCase("FFFFFFF")]
-        public void Book_ToString_InvaliFormat(string format)
+        public void Book_ToString_InvalidData_Test(string format)
         {
             Assert.Throws<FormatException>(() => this.book.ToString(format));
         }
 
         [TestCase("{0:IANP}", ExpectedResult = "978-0735667457 Richter CLR via C# 176")]
         [TestCase("{0:Ianp}", ExpectedResult = "978-0735667457 Richter CLR via C# 176")]
-        public string ToStringCustomBookFormatter_GetStringTests(string format)
+        public string Book_ToString_BookFormatter_ValidData_Test(string format)
         {           
             return string.Format(new BookFormatter(), format, this.book);
         }
 
         [TestCase("{0:inap}")]
-        public void ToStringCustomBookFormatter_FormatExceptionTests(string format)
+        public void Book_ToString_BookFormatter_InvalidData_Tests(string format)
         {
             Assert.Throws<FormatException>(() => string.Format(new BookFormatter(), format, this.book));
         }
